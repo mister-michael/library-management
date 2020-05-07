84c3cf641629a1a2b8383467bc00ef9436f6650a
Michael Clark <michaelclarknashville@protonmail.com>
2020-05-07 12:43:40 -0500

    completed chapter 4

---
+++ libraryproject/libraryapp/views/connection.py
@@ -0,0 +1,2 @@
+class Connection:
+    db_path = "/Users/d.a.n.k.e./workspace/backEnd/book3/library-management/libraryproject/db.sqlite3"
---
+++ libraryproject/libraryapp/views/libraries/list.py
@@ -0,0 +1,57 @@
+import sqlite3
+from django.shortcuts import render, redirect, reverse
+from libraryapp.models import Library
+from ..connection import Connection
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def list_libraries(request):
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select 
+                l.id, 
+                l.title, 
+                l.address 
+            FROM libraryapp_library l;
+            """)
+
+            all_libraries = []
+            dataset = db_cursor.fetchall()
+
+            for row in dataset:
+                lib = Library()
+                lib.id = row["id"]
+                lib.title = row["title"]
+                lib.address = row["address"]
+
+                all_libraries.append(lib)
+
+        template_name = 'libraries/list.html'
+
+        context = {
+            'all_libraries': all_libraries
+        }
+
+        return render(request, template_name, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO libraryapp_library
+            (
+                title, address
+            )
+            VALUES (?, ?)
+            """,
+            (form_data['title'], form_data['address']))
+
+        return redirect(reverse('libraryapp:libraries'))
+
